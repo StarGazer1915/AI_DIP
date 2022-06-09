45f841f7f97bf0fa07b6576dac7c04eff069b986
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-06-09 15:07:31 +0200

    Created methods for classes, created first iteration of Paxos (WIP)

--- Opdracht_3/main.py
+++ Opdracht_3/main.py
@@ -1,14 +1,75 @@
 from objects import Computer, Message, Network
 
 
-def simulate():
-    P = [Computer("p1", "Proposer"), Computer("p2", "Proposer"), Computer("p3", "Proposer")
-         Computer("p4", "Proposer"), Computer("p5", "Proposer"), Computer("p6", "Proposer")]
-    A = [Computer("a1", "Acceptor"), Computer("a2", "Acceptor"), Computer("a3", "Acceptor")
-         Computer("a4", "Acceptor"), Computer("a5", "Acceptor"), Computer("a6", "Acceptor")]
+def create_computers(c_num, c_type):
+    tmp = []
+    for i in range(1, c_num+1):
+        tmp.append(Computer(f"{c_type[0]}{i}", c_type))
+    return tmp
 
-    N = Network("n1")
+
+def simulate(n_p, n_a, tmax, coms):
+    global props
+
+    P = create_computers(n_p, "PROPOSER")
+    A = create_computers(n_a, "ACCEPTOR")
+    N = Network("N1")
+
+    for t in range(tmax):
+        if len(N.queue) == 0 and len(coms) == 0:
+            return
+
+        try:
+            com = coms[t]
+        except:
+            com = None
+
+        if com is not None:
+            e = com
+            if "END" in com:
+                continue
+
+            elif "PROPOSE" in com:
+                # Example: [0, "PROPOSE", 1, 42]
+                e = [com[0], [], [], P[com[2] - 1], com[3]]
+
+            elif "FAIL PROPOSER" in com:
+                # Example: [8, "FAIL PROPOSER", 1]
+                e = [com[0], [P[com[2] - 1]], [], None, None]
+
+            elif "RECOVER PROPOSER" in com:
+                # Example: [26, "RECOVER PROPOSER", 1]
+                e = [com[0], [], [P[com[2] - 1]], None, None]
+
+            coms.remove(com)
+            (t, F, R, pi_c, pi_v) = e
+            for c in F:
+                c.failed = True
+            for c in R:
+                c.failed = False
+
+            if pi_v is not None and pi_c is not None:  # PROPOSE
+                m = Message(None, pi_c, "PROPOSE", pi_v)
+                pi_c.deliver_message(m, t)
+                for a_c in A:
+                    m = Message(pi_c, a_c, "PREPARE", pi_v)
+                    N.queue_message(m)
+                props += 1
+        else:
+            pass
+            m = N.extract_message()
+            if m is not None:
+                m.dst.deliver_message(m, t)
 
     return
 
-simulate()
+
+# =============== EXECUTION =============== #
+commands = [
+    [0, "PROPOSE", 1, 42],
+    [0, "END"]
+]
+
+props = 1
+
+simulate(1, 3, 15, commands)
--- Opdracht_3/objects.py
+++ Opdracht_3/objects.py
@@ -4,30 +4,31 @@ class Computer:
         self.id = id
         self.type = type
         self.failed = False
+        self.value = 0
 
-    def deliver_message(self, c, message):
+    def deliver_message(self, m, tick):
+        # m = [src, dst, type, value]
+        if m.type == "PROPOSE":
+            print(f"{tick}:   -> {m.dst.id} | {m.type} v={m.value}")
+            m.dst.value = m.value
+        elif m.type == "PREPARE":
+            print(f"{tick}: {m.src.id} -> {m.dst.id} | {m.type} n={m.src.id[1]}")
+
         return
 
     def __str__(self):
-        return f"Computer | ID: {self.id} | Type: {self.type} | Failed: {self.failed}"
+        return f"Computer | ID: {self.id} | Type: {self.type} | Failed: {self.failed} | Value: {self.value}"
 
 
 class Message:
-    def __init__(self, msrc, mdst, mtype):
+    def __init__(self, msrc, mdst, mtype, value):
         self.src = msrc
         self.dst = mdst
         self.type = mtype
-
-        # MESSAGE TYPES:
-        # - PROPOSE
-        # - PREPARE
-        # - PROMISE
-        # - ACCEPT
-        # - ACCEPTED
-        # - REJECTED
+        self.value = value
 
     def __str__(self):
-        return f"Message: {self.src}, {self.dst}, {self.type}"
+        return f"Message: {self.src}, {self.dst}, {self.type}, {self.value}"
 
 
 class Network:
@@ -40,7 +41,10 @@ class Network:
         return self.queue
 
     def extract_message(self):
-        message = self.queue[0]
+        try:
+            message = self.queue[0]
+        except:
+            return None
         if not message.src.failed and not message.dst.failed:
             self.queue.pop(0)
             return message
