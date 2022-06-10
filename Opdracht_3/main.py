73b422a929308fb4d8c46bed5b760ced0849126c
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-06-10 15:42:22 +0200

    Finished Learner class and functionality

--- Opdracht_3/main.py
+++ Opdracht_3/main.py
@@ -1,42 +1,78 @@
 from objects import Computer, Message, Network
 
 
-def create_computers(c_num, c_type, N, A):
+def create_computers(c_num, c_type, N, A, L):
+    """
+    This function creates an array of computers for use in the paxos algorithm.
+    This function can be used to create proposers, acceptors and learners.
+    It assigns the Network queue to the computer so it is linked with it at all
+    time and can queue new messages if needed.
+    @param c_num: int
+    @param c_type: str
+    @param N: Network object
+    @param A: list (Computer objects)
+    @param L: list (Computer objects)
+    @return:
+    """
     tmp = []
     for i in range(1, c_num + 1):
-        tmp.append(Computer(f"{c_type[0]}{i}", c_type, N, A))
+        tmp.append(Computer(f"{c_type[0]}{i}", c_type, N, A, L))
     return tmp
 
 
-def simulate(n_p, n_a, tmax, coms):
+def simulate(n_p, n_a, n_l, tmax, coms):
+    """
+    1. First the Network, lists with computers and proposer id counter are initialized.
+    2. Then the simulation starts and it will loop until the max tick (tmax) has been reached.
+    3. The tick is formatted to be used in print statements and the first command is taken from coms.
+        3.1 If a command doesn't exist it gives com the value None instead.
+    4. If the command exists it will format the command into the proper event.
+    5. Now that the event is made the command is removed from the command list and the failed machines
+       are put to sleep or the to be recovered machines are recovered.
+    6. If the event is a PROPOSE event it will queue that message to propose the value.
+    7. Then a message from the queue is extracted and delivered to it's destination if the message exists.
+    8. Eventually (after the simulation loop) the majority is calculated and the consensus result is shown.
+    @param n_p: int
+    @param n_a: int
+    @param n_l: int
+    @param tmax: int
+    @param coms: list
+    @return: void
+    """
     num_prop = 0
 
+    #= 1 =#
     N = Network("N1")
-    A = create_computers(n_a, "ACCEPTOR", N, [])
-    P = create_computers(n_p, "PROPOSER", N, A)
+    A = create_computers(n_a, "ACCEPTOR", N, [], [])
+    L = create_computers(n_l, "LEARNER", N, A, [])
+    P = create_computers(n_p, "PROPOSER", N, A, L)
 
+    #= 2 =#
     for t in range(tmax):
         if len(N.queue) == 0 and len(coms) == 0:
             return
 
+        #= 3 =#
+        tick = str(t).zfill(len(str(tmax)))
         com = coms[0]
         if com[0] != t:
             com = None
 
+        #= 4 =#
         if com:
             e = com
             if "END" in com:
                 continue
-
             elif "PROPOSE" in com:
                 e = [com[0], [], [], P[com[2] - 1], com[3]]
             elif "FAIL PROPOSER" in com:
-                print(f"{str(t).zfill(3)}: ** P{com[2]}  kapot **")
+                print(f"{tick}: ** P{com[2]}  kapot **")
                 e = [com[0], [P[com[2] - 1]], [], None, None]
             elif "RECOVER PROPOSER" in com:
-                print(f"{str(t).zfill(3)}: ** P{com[2]}  gerepareerd **")
+                print(f"{tick}: ** P{com[2]}  gerepareerd **")
                 e = [com[0], [], [P[com[2] - 1]], None, None]
 
+            #= 5 =#
             coms.remove(com)
             (t, F, R, pi_c, pi_v) = e
             for c in F:
@@ -44,15 +80,18 @@ def simulate(n_p, n_a, tmax, coms):
             for c in R:
                 c.failed = False
 
+            #= 6 =#
             if pi_v and pi_c:
                 num_prop += 1
                 pi_c.p_id = num_prop
                 N.queue_message(Message(None, pi_c, "PROPOSE", pi_v))
 
-        m = N.extract_message(t)
+        #= 7 =#
+        m = N.extract_message(tick)
         if m:
-            m.dst.deliver_message(m, t)
+            m.dst.deliver_message(m, tick)
 
+    #= 8 =#
     tmp = [a.value for a in A]
     print("")
     for p in P:
@@ -78,5 +117,20 @@ commands2 = [
     [0, "END"]
 ]
 
-# simulate(1, 3, 15, commands1)
-simulate(2, 3, 50, commands2)
+commands3 = [
+    [0, "PROPOSE", 1, "nl: g"],
+    [100, "PROPOSE", 1, "nl:ga"],
+    [200, "PROPOSE", 1, "nl:af"],
+    [300, "PROPOSE", 1, "nl:f"],
+    [400, "PROPOSE", 1, "en: g"],
+    [500, "PROPOSE", 1, "en:gr"],
+    [600, "PROPOSE", 1, "en:re"],
+    [700, "PROPOSE", 1, "en:ea"],
+    [800, "PROPOSE", 1, "en:at"],
+    [900, "PROPOSE", 1, "en:t"],
+    [0, "END"]
+]
+
+# simulate(1, 3, 0, 15, commands1)
+simulate(2, 3, 0, 50, commands2)
+# simulate(1, 3, 1, 10000, commands3)
--- Opdracht_3/objects.py
+++ Opdracht_3/objects.py
@@ -1,24 +1,75 @@
 class Computer:
-    def __init__(self, id, type, N, A):
+    def __init__(self, id, type, N, A, L):
+        """
+        Defining class values.
+        @param id: str
+        @param type: str
+        @param N: Network object
+        @param A: list (Computer objects)
+        @param L: list (Computer objects)
+        """
         self.id = id
         self.type = type
         self.failed = False
         self.value = None
         self.org_value = None
-        self.p_id = None
-        self.N = N
-        self.A = A
+        self.p_id = None  # Proposer position_id (n=)
+        self.N = N  # Network
+        self.A = A  # Acceptors
+        self.L = L  # Learners
         self.prior = None
+        self.acc_count = 0
         self.rej_count = 0
 
     def deliver_message(self, m, tick):
-        tick = str(tick).zfill(3)
+        """
+        This function handles all possible messages between computers.
+        - PROPOSE
+          The code updates the values in the PROPOSER and writes queue messages for
+          the delivery of PREPARE messages to ACCEPTORS
+
+        - PREPARE
+          The code first checks if the p_id of the ACCEPTOR is lower or equal to
+          the PROPOSER's p_id. If not then it's messages are ignored. If it is, then PROMISE
+          messages are queued.
+
+        - PROMISE
+          The code first checks if the ACCEPTOR already has prior accepted values. If so then
+          the value of the PROPOSER is updated with that correct value. Then the ACCEPT
+          messages are queued.
+
+        - ACCEPT
+          The code first checks if the ACCEPTOR has a None value
+          OR
+          If the ACCEPTOR's value and PROPOSER's value are the same
+          AND
+          The PROPOSER's p_id is larger or equal to the ACCEPTOR's p_id.
+          If so then the value can be accepted and ACCEPTED messages are queued, the prior values
+          are also replaced with the new values. If not then the message is rejected which means
+          that REJECTED messages are queued instead.
+
+        - ACCEPTED
+          The code first assigns the new accepted value to the ACCEPTOR. A PROPOSER's counter is updated
+          to see if a majority of the acceptors have already ACCEPTED the value. If so then SUCCESS messages
+          are queued for the LEARNER computers and the counter is set to zero to avoid problems with other proposals.
+
+        - REJECTED
+          Firstly the REJECTED counter is updated. Then it checks if the majority of the ACCEPTORS have
+          REJECTED the messages. If so, then the PROPOSER tries again and PREPARE messages are queued under a new p_id.
+          Here the counter is also reset to avoid problems with other proposals.
+
+        - SUCCESS
+          The code first updates the value of the LEARNER and shows what majority has been reached.
+
+        @param m: Message object
+        @param tick: int
+        @return: void
+        """
         if m.type == "PROPOSE" and m.dst.type == "PROPOSER":
             print(f"{tick}:    -> {m.dst.id} | {m.type} v={m.value}")
-            m.dst.value = m.value
-            m.dst.org_value = m.value
-
-            for a_c in self.A:
+            m.dst.value, m.dst.org_value = m.value, m.value
+            m.dst.acc_count, m.dst.rej_count = 0, 0
+            for a_c in m.dst.A:
                 self.N.queue_message(Message(m.dst, a_c, "PREPARE", m.value))
                 a_c.p_id = m.dst.p_id
 
@@ -43,6 +94,15 @@ class Computer:
             else:
                 self.N.queue_message(Message(m.dst, m.src, "REJECTED", m.value))
 
+        elif m.type == "ACCEPTED" and m.dst.type == "PROPOSER":
+            m.src.value = m.dst.value
+            m.dst.acc_count += 1
+            print(f"{tick}: {m.src.id} -> {m.dst.id} | {m.type} n={m.dst.p_id} v={m.src.value}")
+            if m.dst.acc_count >= round(len(m.dst.A) / 2):
+                for l_c in m.dst.L:
+                    self.N.queue_message(Message(m.dst, l_c, "SUCCESS", m.dst.org_value))
+                m.dst.acc_count = 0
+
         elif m.type == "REJECTED" and m.dst.type == "PROPOSER":
             print(f"{tick}: {m.src.id} -> {m.dst.id} | {m.type} n={m.dst.p_id}")
             m.dst.rej_count += 1
@@ -52,21 +112,28 @@ class Computer:
                     m.dst.p_id = a_c.p_id + 1
                 m.dst.rej_count = 0
 
-        elif m.type == "ACCEPTED" and m.dst.type == "PROPOSER":
-            m.src.value = m.dst.value
-            print(f"{tick}: {m.src.id} -> {m.dst.id} | {m.type} n={m.dst.p_id} v={m.src.value}")
+        elif m.type == "SUCCESS" and m.dst.type == "LEARNER":
+            m.dst.value = m.src.value
+            print(f"{tick}: {m.src.id} -> {m.dst.id} | {m.type} n={m.src.p_id} | Majority consensus reached on: "
+                  f"{m.src.value}")
 
         else:
             raise RuntimeError(f"Error, combination of {m.type} and {m.dst.type} does not match")
 
-        return
-
     def __str__(self):
-        return f"Computer | ID: {self.id} | Type: {self.type} | Prop_id: {self.p_id} | Failed: {self.failed} | Value: {self.value}"
+        return f"Computer | ID: {self.id} | Type: {self.type} | Prop_id: {self.p_id} | Failed: {self.failed} | " \
+               f"Value: {self.value}"
 
 
 class Message:
     def __init__(self, msrc, mdst, mtype, value):
+        """
+        Defining class values.
+        @param msrc: Computer object or None
+        @param mdst: Computer object or None
+        @param mtype: str
+        @param value: int or str
+        """
         self.src = msrc
         self.dst = mdst
         self.type = mtype
@@ -78,14 +145,30 @@ class Message:
 
 class Network:
     def __init__(self, id):
+        """
+        Defining class value.
+        @param id: str
+        """
         self.id = id
         self.queue = []
 
     def queue_message(self, message):
+        """
+        Places a message at the END of the queue.
+        @param message: Message object
+        @return: void
+        """
         self.queue.append(message)
-        return self.queue
 
     def extract_message(self, t):
+        """
+        Extracts a message from the front of the queue where the source computer and desination computer
+        haven't shut down (failed) OR if there is no source destination (then it is a proposal).
+        After the message is extracted the message is removed from the queue. If there was no available
+        message queued then the function will print a blank line with only the tick.
+        @param t: int
+        @return: Message object or None
+        """
         if not self.queue:
             return None
 
@@ -96,11 +179,8 @@ class Network:
                 return i
             count += 1
 
-        print(f"{str(t).zfill(3)}: ")
+        print(f"{t}: ")
         return None
 
     def __str__(self):
-        print(f"Network: {self.id}")
-        for i in range(len(self.queue)):
-            print(f"{i}: {self.queue[i]}")
-        return ""
+        return f"Network: {self.id} | Queue:\n{self.queue}"
